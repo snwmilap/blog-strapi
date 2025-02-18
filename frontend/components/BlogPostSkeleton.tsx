c82eae194856494e04a9ea2d32986ad5cb22659c
snwmilap <milap@softnweb.in>
2025-02-18 19:12:37 +0530

    feat: add skeleton

---
+++ frontend/components/BlogPostSkeleton.tsx
@@ -0,0 +1,8 @@
+const BlogPostSkeleton = () => {
+    return (
+      <div className="animate-pulse bg-gray-200 rounded-lg h-64 w-full"></div>
+    );
+  };
+  
+  export default BlogPostSkeleton;
+  
---
+++ frontend/components/HeroSectionSkeleton.tsx
@@ -0,0 +1,8 @@
+const HeroSectionSkeleton = () => {
+    return (
+      <div className="animate-pulse bg-gray-300 rounded-xl h-96 w-full mb-6"></div>
+    );
+  };
+  
+  export default HeroSectionSkeleton;
+  
