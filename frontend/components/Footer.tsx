304ddd2ae2630e8a97dccceb194de7cd0e26ac50
snwmilap <milap@softnweb.in>
2025-02-28 17:53:17 +0530

    Update Footer.tsx

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="border-t dark:border-white/20 mt-8">
+    <footer className="border-t dark:border-white/20">
       <div className="container mx-auto">
         <div className="py-8  text-center">
           <p className="text-sm ">© 2025 MiniBlog. All rights reserved.</p>
